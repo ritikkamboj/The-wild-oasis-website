702df28e2fc242e1535770d9bb806452c71b124a
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-04 23:15:41 +0530

    used next JS Image tag to use optimize and responsive image

--- app/page.js
+++ app/page.js
@@ -1,12 +1,25 @@
 import Link from "next/link";
 import Navigation from "./_component/Navigation";
+import Image from "next/image";
+import bg from '@/public/bg.png';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Wild Oasis , Explore the unexplored </h1>
+    <main className="mt-24">
+      <Image placeholder="blur" src={bg} fill className="object-cover object-top" quality={80} alt="Mountains and forests with two cabins" />
 
-    </div>
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
 
   );
 }
