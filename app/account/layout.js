4a3ca5c441270007dba837d3e98492d786c8cc01
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-05 23:23:02 +0530

    working on nested layouts

---
+++ app/account/layout.js
@@ -0,0 +1,6 @@
+export default function Layout({ children }) {
+    return <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+        <div>Navigation </div>
+        <div>{children}</div>
+    </div>
+}
